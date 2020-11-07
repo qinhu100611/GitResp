f1271863b793dd790fefc8d972cfe72078c77e39
qinh <1134609679@qq.com>
2020-11-07 16:17:44 +0800

    IDE集成git的第一个项目

---
+++ src/com/qh/Test01.java
@@ -0,0 +1,9 @@
+package com.qh;
+
+public class Test01 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("helloworld");
+        }
+    }
+}
